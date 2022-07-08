48e267ab6ff17f791b2bcae48b4d9c3415829b7a
Maryam Yusuf <maryam.m.yusuf1802@gmail.com>
2022-07-08 11:37:58 +0100

    add config giles

--- set_env.sh
+++ set_env.sh
@@ -1,8 +1,8 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_USERNAME=appdbuser
+export POSTGRES_PASSWORD=adminadmin
+export POSTGRES_HOST=postgres.cmlnoj6ph0dp.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=arn:aws:s3:::mybucket1200798
 export AWS_REGION=us-east-1
